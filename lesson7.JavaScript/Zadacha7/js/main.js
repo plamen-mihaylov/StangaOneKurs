6e10eeaf0e624017f08d2e2f46c39f9be15f67da
plamen-mihaylov <plamen.mihaylov@gmail.com>
2015-12-15 13:56:41 +0200

    homework7

--- lesson7.JavaScript/Zadacha7/js/main.js
+++ lesson7.JavaScript/Zadacha7/js/main.js
@@ -1,24 +1,22 @@
+function OnFocusInput() {
 
+	var x = document.querySelectorAll('input');
 
-	function OnFocusInput(x,y) {
+	for ( i=0; i < x.length; i++ ) {
 
-		var x = document.querySelectorAll('input');
-    	var i;
-    	for(i=0; i<x.length; i++)
-			{   
-				x[i].parentNode.style.backgroundColor= "#d2d2d2";
-	 		}
-	 		var y = document.activeElement.style.backgroundColor= "#aaf7f4";
-	 		var y = document.activeElement.style.width= "100%";
-	
+		x[i].parentNode.style.backgroundColor= "#d2d2d2";
 
+	}
 
+	document.activeElement.style.backgroundColor= "#aaf7f4";
+	document.activeElement.style.width= "100%";
 
 // var form = document.getElementById("form");
 // form.addEventListener("focus", function( event ) {
 //  event.target.style.background = "pink";    
 // }, true);
-	
-	}
-			
-	
+
+
+}
+
+
