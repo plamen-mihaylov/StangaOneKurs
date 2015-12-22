e2e93760194f66ffb6a0822cd7051735ccf989a7
plamen-mihaylov <plamen.mihaylov@gmail.com>
2015-12-22 12:06:17 +0200

    homework8

---
+++ lesson8.JavaScript/Zadacha8/js/main.js
@@ -0,0 +1,20 @@
+ document.addEventListener("DOMContentLoaded", function(event) {
+
+numbers = [];
+while (numbers.length < 6) {
+    var select = Math.floor(Math.random() * 48) + 1;
+   if (numbers.indexOf(select) < 0)  {
+      numbers.push(select)
+      alert ('Изтегленото числото е: '+select)
+   }
+
+   document.getElementById('first').innerHTML=numbers[0]
+   document.getElementById('second').innerHTML=numbers[1]
+   document.getElementById('thirth').innerHTML=numbers[2]
+   document.getElementById('fourth').innerHTML=numbers[3]
+   document.getElementById('fifth').innerHTML=numbers[4]
+   document.getElementById('sixth').innerHTML=numbers[5]
+}
+
+alert ('Честито на печелившите')
+});
